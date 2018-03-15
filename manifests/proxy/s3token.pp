58c752536872f76d19c8626271caf78fcf5ab26d
Raymond Kristiansen <raymond.kristiansen@it.uib.no>
2018-03-15 14:16:24 +0100

    Update s3token to use new auth_uri and added option for extra settings

--- manifests/proxy/s3token.pp
+++ manifests/proxy/s3token.pp
@@ -4,17 +4,9 @@
 #
 # === Parameters
 #
-# [*auth_host*]
-#   (optional) The keystone host
-#   Defaults to 127.0.0.1
-#
-# [*auth_port*]
-#   (optional) The Keystone client API port
-#   Defaults to 5000
-#
-# [*auth_protocol*]
-#   (optional) http or https
-#    Defaults to http
+# [*auth_uri*]
+#   (optional) The keystone auth uri
+#   Defaults to http://127.0.0.1:35357/
 #
 # == Dependencies
 #
@@ -29,17 +21,21 @@
 # Copyright 2012 eNovance licensing@enovance.com
 #
 class swift::proxy::s3token(
-  $auth_host = '127.0.0.1',
-  $auth_port = '35357',
-  $auth_protocol = 'http'
+  $auth_uri = 'http://127.0.0.1:35357/',
+  $auth_version = '2.0',
+  $insecure = false,
+  $reseller_prefix = 'AUTH_',
+  $delay_auth_decision = false
 ) {
 
   include ::swift::deps
 
   swift_proxy_config {
-    'filter:s3token/paste.filter_factory': value => 'keystonemiddleware.s3_token:filter_factory';
-    'filter:s3token/auth_host':            value => $auth_host;
-    'filter:s3token/auth_port':            value => $auth_port;
-    'filter:s3token/auth_protocol':        value => $auth_protocol;
+    'filter:s3token/use':                 value => 'egg:swift3#s3token';
+    'filter:s3token/auth_uri':            value => $auth_uri;
+    'filter:s3token/auth_version':        value => $auth_version;
+    'filter:s3token/insecure':            value => $insecure;
+    'filter:s3token/reseller_prefix':     value => $reseller_prefix;
+    'filter:s3token/delay_auth_decision': value => $delay_auth_decision;
   }
 }
